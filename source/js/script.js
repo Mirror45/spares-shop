d2030a4ac02de60400ed191a6c0c6baa831bbab2
Mirror <yllikano@gmail.com>
2024-08-23 12:22:42 +0300

    js fix

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,25 @@
-import { accordeon } from './footer-accordeon.js';
+'use strict';
 
-accordeon();
+
+// Accordeon
+
+(function () {
+  const footerToggles = document.querySelectorAll('.page-footer__accordeon-container button');
+  const pageFooterSections = document.querySelectorAll('.page-footer__sections div');
+
+  footerToggles.forEach(function (footerToggle) {
+    footerToggle.addEventListener('click', onToggleClicked);
+  });
+
+  function onToggleClicked(e) {
+    const section = e.target.closest('.page-footer__accordeon-container').parentElement;
+    if (section.classList.contains('page-footer__accordeon--opened')) {
+      section.classList.remove('page-footer__accordeon--opened');
+    } else {
+      pageFooterSections.forEach(function (pageFooterSection) {
+        pageFooterSection.classList.remove('page-footer__accordeon--opened');
+      });
+      section.classList.add('page-footer__accordeon--opened');
+    }
+  }
+})();
