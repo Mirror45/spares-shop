07b3cf3093f9475ccbdc9cfc54ae304aa309043d
Mirror <yllikano@gmail.com>
2024-08-31 15:05:00 +0300

    wares section html/style/js

--- source/js/main/slider.js
+++ source/js/main/slider.js
@@ -26,6 +26,72 @@ const slickSlider = () => {
         }
       }]
     });
+
+    const waresPopularSlider = $('.wares--popular .wares__list');
+    waresPopularSlider.slick({
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      dots: false,
+      arrows: true,
+      responsive: [{
+        breakpoint: 1232,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          arrows: false,
+          dots: true,
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          dots: true,
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }]
+    });
+
+    const waresSuggestionSlider = $('.wares--suggestion .wares__list');
+    waresSuggestionSlider.slick({
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      dots: false,
+      arrows: true,
+      responsive: [{
+        breakpoint: 1232,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          arrows: false,
+          dots: true,
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          dots: true,
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }]
+    });
   }
 };
 
