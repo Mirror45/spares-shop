024b38e1badf445122888fbda87dfdb3e210c181
Mirror <yllikano@gmail.com>
2024-09-19 12:48:45 +0300

    delete slick-slider

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,5 @@
 import { menu } from "./main/menu.js"
 import { accordeon } from './main/accordeon.js';
-import { slickSlider } from './main/slider.js';
 import { tabs } from './main/tabs.js';
 import { favorite } from './main/favorite.js';
 import { productRating } from './main/product-rating.js';
@@ -9,7 +8,6 @@ import { filter } from './main/filters.js';
 
 menu();
 accordeon();
-slickSlider();
 tabs();
 favorite();
 productRating();
